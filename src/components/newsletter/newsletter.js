7c5dc233c870291957146a76f570a7359c65d50a
Victor <victorlaucas@gmail.com>
2018-05-04 13:46:49 -0600

    Setup newsletter archive

--- src/components/newsletter/newsletter.js
+++ src/components/newsletter/newsletter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import NewsletterArchive from './newsletter_archive';
 
 class Newsletter extends Component {
 
@@ -11,7 +12,15 @@ class Newsletter extends Component {
     render() {
         return (
             <div>
-                
+                <div>
+                      <div>
+                        02 jan 18
+                      </div>  
+                       <NewsletterArchive/>
+                </div>
+                <div>
+                    main content. the latest newsletter.
+                </div>
             </div>
         )
     }
---
+++ src/components/newsletter/newsletter_archive.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NewsletterArchive extends Component {
+    render() {
+        return (
+            <ul>
+                <li>list item 0</li>
+                <li>list item 1</li>
+                <li>list item 2</li>
+            </ul>
+        )
+    }
+}
+
+export default NewsletterArchive;
