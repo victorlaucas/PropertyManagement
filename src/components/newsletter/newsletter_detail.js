e06c45683490b9aba2f131201d1a18425960fabe
Victor <victorlaucas@gmail.com>
2018-05-04 14:55:10 -0600

    added newsletter details component and corresponding route

---
+++ src/components/newsletter/newsletter_detail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsletterDetail extends Component {
+    render() {
+        return (
+            <div>
+                newsletter detail
+            </div>
+        )
+    }
+}
+
+export default NewsletterDetail;
