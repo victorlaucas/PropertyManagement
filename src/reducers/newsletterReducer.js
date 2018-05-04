4f734af7362806f53c19a0595199ff0dbf782078
Victor <victorlaucas@gmail.com>
2018-05-04 14:45:32 -0600

    Fixed archive compoent

--- src/reducers/newsletterReducer.js
+++ src/reducers/newsletterReducer.js
@@ -3,10 +3,13 @@ import { FETCH_NEWSLETTER_ARCHIVE } from "../actions/types";
 export default function(state = {}, action) {
     switch(action.type) {
         case FETCH_NEWSLETTER_ARCHIVE:
+            const latestItem = action.payload[0];
+            const archive = action.payload;
+            archive.shift();
             return { 
                 ...state,
-                archive: action.payload,
-                latestItem: action.payload[0]
+                archive,
+                latestItem
              }
         default: return state;
     }
