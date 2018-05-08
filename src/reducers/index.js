721e49258fb5e99bf44c8697eb55c76cef57f5b9
Victor <victorlaucas@gmail.com>
2018-05-08 14:31:41 -0600

    Added redux dev tools and connected support request reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import newsletterReducer from './newsletterReducer';
+import supportRequestReducer from './supportRequestReducer';
 
 const rootReducer = combineReducers({
   form,
-  newsletter: newsletterReducer
+  newsletter: newsletterReducer,
+  supportRequest: supportRequestReducer
 });
 
 export default rootReducer;
