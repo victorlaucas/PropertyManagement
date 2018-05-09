9d60de0459fe42f44bb34bbdd6d8fb9fc3af98ec
Victor <victorlaucas@gmail.com>
2018-05-09 16:37:03 -0600

    Added header styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Header</h1>
+      <div className="main-header">
+        <h1>Welcome to The HOA Manager!</h1>
+        <p>Please login to continue</p>
         {this.props.children}
       </div>
     );
   }
 }
-
 
 export default Header;
