33253cacd361c693bbaf943acf431289083fcce8
Victor <victorlaucas@gmail.com>
2018-05-11 14:06:31 -0600

    Added remaining addHeaderNav styles

--- src/components/newsletter/newsletter.js
+++ src/components/newsletter/newsletter.js
@@ -16,15 +16,15 @@ class Newsletter extends Component {
 
     render() {
         return (
-            <div className="row">
-                <div className="col-md-3">
+            <div className="newsletter">
+                <div className="newsletter__side">
                     <div style={{width: '200px', height: '200px', backgroundColor: 'skyblue'}}>
                         02 jan 18
                     </div> 
 
                     <NewsletterArchive archive={this.props.archive}/>
                 </div>
-                <div className="col-md-9">
+                <div>
                     <NewsletterContent latestItem={this.props.latestItem} />
                 </div>
             </div>
