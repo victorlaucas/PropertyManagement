40b42930e542401d2e3a63fabc5e23c13aa74ff9
Victor <victorlaucas@gmail.com>
2018-05-11 15:41:42 -0600

    Styled infoBox

--- src/components/infoBox.js
+++ src/components/infoBox.js
@@ -11,11 +11,16 @@ class InfoBox extends Component {
 
     render() {
         return (
-            <div className="info-box">
-                <a onClick={() => this.handleSelected()}>
-                    the type of this InfoBox is {this.props.type}
-                </a>
-            </div>
+            <a onClick={() => this.handleSelected()}>
+                <div className={`info-box`}>
+                    <div className="info-box__text">
+                        02
+                    </div>
+                    <div className="info-box__sub-text">
+                        {this.props.type}
+                    </div>
+                </div> 
+            </a>
         )
     }
 }
--- src/components/newsletter/newsletter.js
+++ src/components/newsletter/newsletter.js
@@ -18,8 +18,13 @@ class Newsletter extends Component {
         return (
             <div className="newsletter">
                 <div className="newsletter__side">
-                    <div style={{width: '200px', height: '200px', backgroundColor: 'skyblue'}}>
-                        02 jan 18
+                    <div className="info-box">
+                        <div className="info-box__text">
+                            02
+                        </div>
+                        <div className="info-box__sub-text">
+                            JAN 2018
+                        </div>
                     </div> 
 
                     <NewsletterArchive archive={this.props.archive}/>
