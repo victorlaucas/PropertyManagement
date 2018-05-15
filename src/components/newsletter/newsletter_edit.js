a834eabfcd450bc10048b659cbae525048746daf
Victor <victorlaucas@gmail.com>
2018-05-15 11:05:20 -0600

    Styled newletter edit

--- src/components/newsletter/newsletter_edit.js
+++ src/components/newsletter/newsletter_edit.js
@@ -29,12 +29,23 @@ class EditNewsletter extends Component {
         const { handleSubmit } = this.props;
 
         return (
-            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-                <Field name="title" component={this.renderInput} type="text"/>
-                <Field name="body" component={this.renderInput} type="textarea"/>
-
-                <Link to="/newsletter"><div>Cancel</div></Link>
-                <button action="submit" className="btn btn-primary">Save</button>
+            <form className="add-newsletter-form"onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <div className="add-newsletter-form__title">
+                    <label htmlFor="title">Newsletter Title</label>
+                    <Field name="title" component={(field) => <input {...field.input} />} type="text"/>
+                </div>
+                <div className="add-newsletter-form__body">
+                    <label htmlFor="body">Body</label>
+                    <Field name="body" component={(field) => <textarea {...field.input} />} type="textarea"/>
+                </div>
+         
+                <Link to="/newsletter">
+                    <div className="add-newsletter-form__cancel">
+                        <p>Cancel</p>
+                    </div>
+                </Link>
+   
+                <button className="add-newsletter-form__submit" action="submit">Submit</button>
             </form>
         )
     }
